f75642aa6fe6251c50c5025bc9c5aacba10d077a
joekabar <109431691+joekabar@users.noreply.github.com>
2026-03-21 19:29:58 +0100

    Fix named import for AppRoutes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import BrandingProvider from './context/BrandingProvider'
-import AppRoutes from './routes'
+import { AppRoutes } from './routes'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
